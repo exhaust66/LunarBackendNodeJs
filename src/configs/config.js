f3c34479d7b84f1e0dc2c4c59b9a769e1d65c0a7
exhaust66 <134827458+exhaust66@users.noreply.github.com>
2024-12-27 20:12:28 +0545

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,36 +1,13 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-let db_config
-if (process.env.NODE_MODE === "PRODUCTION") {
-    db_config = {
-        HOST: process.env.DB_HOST,
-        USER: process.env.DB_USER,
-        PASSWORD: process.env.DB_PASS,
-        DB: process.env.DB_NAME,
-        PORT: Number(process.env.DB_PORT),
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-} else {
-    db_config = {
-        HOST: "localhost",
-        USER: "root",
-        PASSWORD: "",
-        DB: "lunar_db",
-        dialect: "mysql",
-        PORT: 3306,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-}
-module.exports = db_config
+module.exports={
+    HOST:"localhost",
+    USER:"root",
+    PASSWORD:"",
+    DB:"lunar_db",
+    dialect:"mysql",
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000,
+    },
+};
